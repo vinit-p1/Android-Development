ea7f504ed6c8538bf284e6e99900ed494b13cb21
Vinit Pahilajani <vinit.pahilajani@gmail.com>
2023-11-05 14:13:45 +0100

    Removed Exception: Connect with theme pending

--- app/src/main/java/com/example/miniproject1/ViewModel.kt
+++ app/src/main/java/com/example/miniproject1/ViewModel.kt
@@ -9,11 +9,13 @@ import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
+private val Context.dataStore by preferencesDataStore("settings")
+
 class ViewModel(private val app: Application) : AndroidViewModel(app) {
-    private val Context.dataStore by preferencesDataStore("settings")
 
     val DARK_THEME_KEY = booleanPreferencesKey("dark_theme_key")
     val DYNAMIC_COLOR_KEY = booleanPreferencesKey("dynamic_color_key")
@@ -22,18 +24,23 @@ class ViewModel(private val app: Application) : AndroidViewModel(app) {
 //        it[userKey] ?: ""
 //    }
     val isDarkTheme: Flow<Boolean> = app.dataStore.data
-        .map { it[DARK_THEME_KEY] ?: false // Provide a default value
+        .map { it[DARK_THEME_KEY] ?: false
         }
 
     val isDynamicColor: Flow<Boolean> = app.dataStore.data
-        .map { it[DYNAMIC_COLOR_KEY] ?: true // Provide a default value
+        .map { it[DYNAMIC_COLOR_KEY] ?: true
         }
 
     fun saveOptionsPreferences(key: Preferences.Key<Boolean>, value: Boolean){
         viewModelScope.launch {
-            app.dataStore.edit {preferences ->
-                preferences[key] = value
+            app.dataStore.edit {
+                it[key] = value
             }
         }
     }
+
+//    suspend fun getOptionPreferences(key: Preferences.Key<Boolean>): Boolean {
+//        val preferences = app.dataStore.data.first()
+//        return preferences[key] ?: false
+//    }
 }
